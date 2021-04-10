0d750f667f6f443c71a16b3ce86a42488677063a
CielPhive <79680910+CielPhive@users.noreply.github.com>
2021-04-10 16:08:20 +0900

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+
+
+def kinro():
+    html_data = requests.get('https://kinro.ntv.co.jp/lineup/')
+    soup = BeautifulSoup(html_data.text, "html.parser")
+
+
+    #最新の金曜ロードショー放送日、タイトルを取得
+    date = soup.find(class_='list').find(class_='date').string
+    title = soup.find(class_='list').find(class_='title').string
+   
+
+    return date, title
